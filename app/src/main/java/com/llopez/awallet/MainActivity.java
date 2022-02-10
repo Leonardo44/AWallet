077b08ed73eed9cc1dfd99394764f3a934eb5741
Leonardo López <leonardo@Leonardos-MacBook-Air.local>
2022-02-09 23:52:25 -0600

    Configuracion de navegacion

--- app/src/main/java/com/llopez/awallet/MainActivity.java
+++ app/src/main/java/com/llopez/awallet/MainActivity.java
@@ -1,14 +1,34 @@
 package com.llopez.awallet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+    }
+
+    private void onDestinationChanged(@NonNull NavController controller,
+                                      @NonNull NavDestination destination, @Nullable Bundle arguments) {
+        if(destination.getId() == R.id.login_fragment) {
+
+        }
     }
 }
