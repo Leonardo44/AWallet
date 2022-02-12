61626342098c846234153c8c6f7844edf054b648
Leonardo López <leonardo@Leonardos-MacBook-Air.local>
2022-02-12 00:29:48 -0600

    add bills fragment, toolbar custom design and splash fragment

--- app/src/main/java/com/llopez/awallet/MainActivity.java
+++ app/src/main/java/com/llopez/awallet/MainActivity.java
@@ -3,6 +3,8 @@ package com.llopez.awallet;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
@@ -10,25 +12,46 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private NavController navController;
+    private DrawerLayout drawerLayout;
+    private AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        appBarLayout = findViewById(R.id.appBarLayout);
+
         final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
+        navController.addOnDestinationChangedListener(this::onDestinationChanged);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        navController.removeOnDestinationChangedListener(this::onDestinationChanged);
     }
 
     private void onDestinationChanged(@NonNull NavController controller,
                                       @NonNull NavDestination destination, @Nullable Bundle arguments) {
-        if(destination.getId() == R.id.login_fragment) {
-
+        if (destination.getId() == R.id.login_fragment) {
+//            appBarLayout.setVisibility(View.GONE);
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        } else if (destination.getId() == R.id.splashFragment) {
+//            appBarLayout.setVisibility(View.GONE);
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+        } else {
+            appBarLayout.setVisibility(View.VISIBLE);
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
         }
     }
 }
