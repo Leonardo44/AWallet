c3820f1131a8ae93df07c732f9089e1c3c6e96ce
ErickAguilar009 <78826807+ErickAguilar009@users.noreply.github.com>
2022-02-12 17:19:49 -0600

    Agregando Bills Adapter para las Lista de Bills

--- app/src/main/java/com/llopez/awallet/ui/list_bills/BillsAdapter.java
+++ app/src/main/java/com/llopez/awallet/ui/list_bills/BillsAdapter.java
@@ -7,6 +7,51 @@ import android.widget.TextView;
 import com.llopez.awallet.R;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class BillsAdapter  {
+public class BillsAdapter  extends RecyclerView.Adapter<BillsAdapter.ViewHolder> {
+    private Integer[] localDataSet;
 
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            textView = view.findViewById(R.id.tvValue);
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public BillsAdapter(Integer[] dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.bill_item_list, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.length;
+    }
 }
