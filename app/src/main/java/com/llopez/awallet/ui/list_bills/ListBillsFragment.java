85f896c2885ef04a2c27ec3d9bfc61b5f6b8fc6a
ErickAguilar009 <78826807+ErickAguilar009@users.noreply.github.com>
2022-02-12 17:26:11 -0600

    Agregar Fragments de Bill List

--- app/src/main/java/com/llopez/awallet/ui/list_bills/ListBillsFragment.java
+++ app/src/main/java/com/llopez/awallet/ui/list_bills/ListBillsFragment.java
@@ -18,6 +18,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.llopez.awallet.R;
 
 public class ListBillsFragment extends Fragment {
+    private RecyclerView rvBills;
+    private FloatingActionButton btnAdd;
+    private BillsAdapter billsAdapter;
+
     public ListBillsFragment() {
     }
 
@@ -30,11 +34,44 @@ public class ListBillsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_list_bills, container, false);
+
+        rvBills = layout.findViewById(R.id.rvBills);
+        btnAdd = layout.findViewById(R.id.floatingActionButton);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToAddBillFragment();
+            }
+        });
+
+        rvBills.addItemDecoration(new DividerItemDecoration(rvBills.getContext(), DividerItemDecoration.VERTICAL));
         return layout;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Integer[] dataSet = new Integer[10];
+        dataSet[0] = 0;
+        dataSet[1] = 1;
+        dataSet[2] = 2;
+        dataSet[3] = 3;
+        dataSet[4] = 4;
+        dataSet[5] = 5;
+        dataSet[6] = 6;
+        dataSet[7] = 7;
+        dataSet[8] = 8;
+        dataSet[9] = 9;
+
+        rvBills.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        billsAdapter = new BillsAdapter(dataSet);
+        rvBills.setAdapter(billsAdapter);
+    }
+
+    private void moveToAddBillFragment() {
+        NavHostFragment.findNavController(this).navigate(R.id.action_listBillsFragment_to_addBillFragment);
     }
 }
+
