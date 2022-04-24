28460f68f74240d94bc41b6d957b0269d7a99aff
ErickAguilar009 <78826807+ErickAguilar009@users.noreply.github.com>
2022-04-23 22:28:32 -0600

    Update ListEarnings fragment

--- app/src/main/java/com/llopez/awallet/ui/list_earnings/ListEarningsFragment.java
+++ app/src/main/java/com/llopez/awallet/ui/list_earnings/ListEarningsFragment.java
@@ -5,22 +5,36 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.llopez.awallet.R;
+import com.llopez.awallet.model.BillObject;
+import com.llopez.awallet.model.EarningObject;
 import com.llopez.awallet.ui.list_bills.BillsAdapter;
+import com.llopez.awallet.ui.list_bills.ListBillsViewModel;
+import com.llopez.awallet.ui.list_bills.ListBillsViewModelFactory;
+import com.llopez.awallet.utilities.GetDataStatus;
 
-public class ListEarningsFragment extends Fragment {
+public class ListEarningsFragment extends Fragment implements EarningAdapter.EarningAdapterAdapterListener {
     private RecyclerView rvEarnings;
     private FloatingActionButton btnAdd;
-    private BillsAdapter billsAdapter;
+    private TextView emptyMessage;
+
+    private EarningAdapter adapter;
+    private EarningListViewModel viewModel;
 
     public ListEarningsFragment() {
     }
@@ -28,6 +42,7 @@ public class ListEarningsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this, new EarningListViewModelFactory(FirebaseAuth.getInstance().getCurrentUser(), FirebaseFirestore.getInstance())).get(EarningListViewModel.class);
     }
 
     @Override
@@ -36,15 +51,38 @@ public class ListEarningsFragment extends Fragment {
         View layout = inflater.inflate(R.layout.fragment_list_earnings, container, false);
         rvEarnings = layout.findViewById(R.id.rvEarnings);
         btnAdd = layout.findViewById(R.id.floatingActionButtonEarnings);
+        emptyMessage = layout.findViewById(R.id.emptyMessage);
 
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moveToAddEarningFragment();
+        emptyMessage.setVisibility(View.VISIBLE);
+
+        btnAdd.setOnClickListener(v -> moveToAddEarningFragment());
+
+        rvEarnings.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+
+        final Observer<GetDataStatus> getDataStatusObserver = newValue -> {
+            switch (newValue) {
+                case INITIALIZE:
+                    break;
+                case LOADING:
+                    break;
+                case ERROR:
+                    break;
+                case SUCCESS:
+                    adapter = new EarningAdapter(viewModel.earningsList, this);
+
+                    if (viewModel.earningsList.isEmpty()) {
+                        emptyMessage.setVisibility(View.VISIBLE);
+                    } else {
+                        emptyMessage.setVisibility(View.GONE);
+                    }
+
+                    rvEarnings.setAdapter(adapter);
+                    break;
+                case NOT_INTERNET:
+                    break;
             }
-        });
-
-        rvEarnings.addItemDecoration(new DividerItemDecoration(rvEarnings.getContext(), DividerItemDecoration.VERTICAL));
+        };
+        viewModel.getCurrentDataStatus().observe(this.getActivity(), getDataStatusObserver);
 
         return layout;
     }
@@ -52,9 +90,18 @@ public class ListEarningsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        viewModel.loadDataFromService();
     }
 
     private void moveToAddEarningFragment() {
         NavHostFragment.findNavController(this).navigate(R.id.action_listEarningsFragment_to_addEarningFragment);
+    }
+
+    @Override
+    public void onEditEarning(EarningObject earning) {
+    }
+
+    @Override
+    public void onDeleteEarning(EarningObject earning) {
     }
 }
